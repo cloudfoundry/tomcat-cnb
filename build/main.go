8ea9a6932ea248bbf09342b57516f694cca2b3ba
Ben Hale <bhale@pivotal.io>
2019-05-02 12:20:31 -0700

    Initial Project Structure
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ build/main.go
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/buildpack/libbuildpack/buildplan"
+	"github.com/cloudfoundry/libcfbuildpack/build"
+)
+
+func main() {
+	build, err := build.DefaultBuild()
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to initialize Build: %s\n", err)
+		os.Exit(101)
+	}
+
+	if code, err := b(build); err != nil {
+		build.Logger.Info(err.Error())
+		os.Exit(code)
+	} else {
+		os.Exit(code)
+	}
+}
+
+func b(build build.Build) (int, error) {
+	return build.Success(buildplan.BuildPlan{})
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/cloudfoundry/tomcat-cnb
+
+go 1.12
+
+require (
+	github.com/buildpack/libbuildpack v1.14.0
+	github.com/cloudfoundry/libcfbuildpack v1.55.0
+	github.com/onsi/gomega v1.5.0
+	github.com/sclevine/spec v1.2.0
+)
