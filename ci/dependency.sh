30428515a3166e2f4f44938557d2b21f239881a0
Ben Hale <bhale@pivotal.io>
2019-05-02 12:34:56 -0700

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/dependency.sh
+++ ci/dependency.sh
@@ -18,10 +18,10 @@ git config --local user.email ${GIT_USER_EMAIL}
 go build -ldflags='-s -w' -o bin/dependency github.com/cloudfoundry/libcfbuildpack/dependency
 
 bin/dependency tomcat "7\.[\d]+\.[\d]+" $(cat ../tomcat-7/version) $(cat ../tomcat-7/uri)  $(cat ../tomcat-7/sha256)
-commit tomcat-7 $(cat ../tomcat-7/version)
+commit tomcat $(cat ../tomcat-7/version)
 
 bin/dependency tomcat "8\.[\d]+\.[\d]+" $(cat ../tomcat-8/version) $(cat ../tomcat-8/uri)  $(cat ../tomcat-8/sha256)
-commit tomcat-8 $(cat ../tomcat-8/version)
+commit tomcat $(cat ../tomcat-8/version)
 
 bin/dependency tomcat "9\.[\d]+\.[\d]+" $(cat ../tomcat-9/version) $(cat ../tomcat-9/uri)  $(cat ../tomcat-9/sha256)
-commit tomcat-9 $(cat ../tomcat-9/version)
+commit tomcat $(cat ../tomcat-9/version)
